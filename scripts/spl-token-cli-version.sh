698d52446ac825133f3bdb2e104b0990d04cefde
hide-yoshi <hide-yoshi@users.noreply.github.com>
2024-10-03 13:19:24 +0900

    remove packages

--- scripts/spl-token-cli-version.sh
+++
@@ -1,8 +0,0 @@
-# populate this on the stable branch
-splTokenCliVersion=3.4.1
-
-maybeSplTokenCliVersionArg=
-if [[ -n "$splTokenCliVersion" ]]; then
-    # shellcheck disable=SC2034
-    maybeSplTokenCliVersionArg="--version $splTokenCliVersion"
-fi
